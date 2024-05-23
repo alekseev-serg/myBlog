83eab18508c1550a36010b18b02708f6fb7d5d50
sergey.alekseev <sergey.alekseev@osinit.com>
2024-05-23 09:30:50 +0300

    add home page

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Админ',
+        'title': 'Это первый пост',
+        'content': 'Содержание первого поста.',
+        'date_posted': '12 мая, 2022'
+    },
+    {
+        'author': 'Пользователь',
+        'title': 'Это второй пост',
+        'content': 'Подробное содержание второго поста.',
+        'date_posted': '13 мая, 2022'
+    }
+]
 
 
-# Create your views here.
 def home(request):
-    return HttpResponse('<h1>Главная</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Наш клуб</h1>')
+    return render(request, 'blog/about.html', {'title': 'О клубе Python Bytes'})
