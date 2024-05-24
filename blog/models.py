52492b21b6b1f0c68b41747d73d8982d38dc8fd3
sergey.alekseev <sergey.alekseev@osinit.com>
2024-05-24 13:13:05 +0300

    add FK models category

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=False)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -17,3 +18,15 @@ class News(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=155, db_index=True, verbose_name='title')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
