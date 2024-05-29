5b5a923ca25809fdb5d0e69966e6bf01272ef356
Алексеев Сергей <sergey.alekseev@osinit.com>
2024-05-29 22:24:29 +0300

    add category and media

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,28 @@
 from django.shortcuts import render
-from .models import News
-
-posts = [
-    {
-        'author': 'Админ',
-        'title': 'Это первый пост',
-        'content': 'Содержание первого поста.',
-        'date_posted': '12 мая, 2022'
-    },
-    {
-        'author': 'Пользователь',
-        'title': 'Это второй пост',
-        'content': 'Подробное содержание второго поста.',
-        'date_posted': '13 мая, 2022'
-    }
-]
+from .models import News, Category
 
 
 def home(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'title': 'Main Page',
-        'news': news
+        'news': news,
+        'categories': categories
     }
     return render(request, 'blog/home.html', context)
 
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'categories': categories,
+        'category': category
+    }
+    return render(request, 'blog/category.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'О клубе Python Bytes'})
